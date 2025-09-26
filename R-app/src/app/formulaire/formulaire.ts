92ea8250dea9737e77980b1b0b709fb49d29b3c7
Nayarr <rayan.tournay@etu.umontpellier.fr>
2025-09-26 10:58:02 +0200

    Passage a un form avec FormControl et FormGroup

--- R-app/src/app/formulaire/formulaire.ts
+++ R-app/src/app/formulaire/formulaire.ts
@@ -1,32 +1,38 @@
 import { Component, output } from '@angular/core';
 import { StudentDto } from '../types/student-dto';  
-
+import { FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+ 
 
 @Component({
   selector: 'app-formulaire',
   standalone : true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './formulaire.html',
   styleUrl: './formulaire.scss'
 })
 
 export class Formulaire {
-  newStudent = output<Omit<StudentDto, 'id'>>();
+  readonly form = new FormGroup({
+    firstname : new FormControl<string >('', {nonNullable : true}),
+    name : new FormControl<string>('', {nonNullable : true}),
+    filiere : new FormControl<string>('', {nonNullable : true}),
+    promo : new FormControl<number>(0, {nonNullable : true}),
+    date : new FormControl<string>('', {nonNullable : true}),
+  })
+
+  newStudent = output<Omit<StudentDto, 'id'>>()
 
   onSubmit(event : Event) : void{
-    event.preventDefault(); //Empêche le rechargement de la page
+    event.preventDefault(); 
 
-    const form = event.target as HTMLFormElement
-    const formData = new FormData(form)
-
-    const newStudent: Omit<StudentDto, 'id'> = {
-      firstname: (formData.get('firstname') as string),
-      name: (formData.get('name') as string),
-      filiere: (formData.get('filiere') as string),
-      promo: Number(formData.get('promo')),
-      date: new Date((formData.get('date') as string))
-    };
-
-    this.newStudent.emit(newStudent);
+    const student : Omit<StudentDto, 'id'> = {
+      firstname : this.form.value.firstname as string,
+      name : this.form.value.name as string,
+      filiere : this.form.value.filiere as string,
+      promo : this.form.value.promo as number,
+      date: new Date(this.form.value.date as string),
+    }
+    
+    this.newStudent.emit(student);
   }
 }
