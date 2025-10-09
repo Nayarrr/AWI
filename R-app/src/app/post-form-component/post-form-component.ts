bd7b76d44b4197c230f1791e9b6f5ba51eb09381
Nayarr <rayan.tournay@etu.umontpellier.fr>
2025-10-09 09:42:16 +0200

    Le big Nabilou a réparé le form de Post !

--- R-app/src/app/post-form-component/post-form-component.ts
+++ R-app/src/app/post-form-component/post-form-component.ts
@@ -1,23 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, output} from '@angular/core';
 import { PostDTO } from '../types/post-dto';
-import { FormControl, FormGroup, NonNullableFormBuilder } from '@angular/forms';
-import { signal } from '@angular/core'
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-post-form-component',
-  imports: [],
+  standalone : true,
+  imports: [ReactiveFormsModule],
   templateUrl: './post-form-component.html',
   styleUrl: './post-form-component.scss'
 })
 
 export class PostFormComponent {
-  readonly form = new FormGroup({
+
+  newP = output<PostDTO>()
+  cancel = output<void>()
+
+  readonly form= new FormGroup({
     userId : new FormControl<number>(0, {nonNullable : true}),
     id : new FormControl<number | undefined>(0, {nonNullable : true}),
     title : new FormControl<string>('', {nonNullable : true}),
     body : new FormControl<string>('', {nonNullable : true})
   })
 
-  posts = signal<PostDTO[]>([])
+  onSubmit(event : Event) : void{
+    event.preventDefault()
+
+    const newPost : PostDTO = {
+      userId : this.form.value.userId as number,
+      id : this.form.value.id as number,
+      title : this.form.value.title as string,
+      body : this.form.value.body as string,
+    }
+    this.newP.emit(newPost)
+  }
+
+  onCancel(): void {
+    this.cancel.emit();
+    this.form.reset({ userId: 0, id: 0, title: '', body: '' });
+  }
   
 }
--- R-app/src/app/post-list-component/post-list-component.ts
+++ R-app/src/app/post-list-component/post-list-component.ts
@@ -3,10 +3,11 @@ import { HttpClient, httpResource } from '@angular/common/http';
 import { PostDTO } from '../types/post-dto';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { JsonPipe } from '@angular/common';
+import { PostFormComponent } from '../post-form-component/post-form-component';
 
 @Component({
   selector: 'app-post-list-component',
-  imports: [JsonPipe],
+  imports: [JsonPipe, PostFormComponent],
   templateUrl: './post-list-component.html',
   styleUrl: './post-list-component.scss'
 })
@@ -17,20 +18,22 @@ export class PostListComponent {
   // TOUT CE QUI EST EN COMMENTAIRE LA ACTUELLEMENT C'EST LE GET PAS LE POST 
   // readonly posts = httpResource<PostDTO[]>(() => ({url : 'https://jsonplaceholder.typicode.com/posts'}))
 
-  postsAPI = toSignal(this.http.get<PostDTO>('https://jsonplaceholder.typicode.com/posts'))
+  showForm = signal(false);
+
+  toggleForm(): void {
+    this.showForm.update(show => !show);
+  }
+
+  postsAPI = toSignal(this.http.get<PostDTO[]>('https://jsonplaceholder.typicode.com/posts'), { initialValue: null })
 
   pNews = signal<PostDTO[]>([])
 
   posts = computed(() => {
-    const plist = this.postsAPI() ?? []
-    return [...plist, ...this.pNews()]
+    return [...this.postsAPI() ?? [], ...this.pNews()]
   })
-  
-  private readonly _posts = signal<PostDTO[]>([
-    {userId : 1, id : 1, title : 'lalala', body : 'papapapapapapapa'}
-  ])
 
   addPost(post : PostDTO) : void{
     this.http.post<PostDTO>('https://jsonplaceholder.typicode.com/posts', post).subscribe(newPost => {this.pNews.update(arr => [...arr , newPost])})
+    this.showForm.set(false)
   }
 }
