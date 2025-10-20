051777cc5c72ac1704d7455574b13a2bdcb4b5cb
Nayarr <rayan.tournay@etu.umontpellier.fr>
2025-10-20 11:06:29 +0200

    Faut faire le front

--- secure-app/frontend/Training/src/app/auth/auth-guard.ts
+++ secure-app/frontend/Training/src/app/auth/auth-guard.ts
@@ -1,5 +1,9 @@
 import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authGuard: CanActivateFn = (route, state) => {
-  return true;
+  const router = inject(Router)
+  if 
+  return true
 };
